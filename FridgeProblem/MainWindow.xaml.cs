b7ec991be2f6daea8570733e2c6367e56a3d21c7
Volodymyr Shonbin <shonbin.volodymyr@gmail.com>
2019-03-16 19:12:57 +0200

    Initial commit

---
+++ FridgeProblem/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FridgeProblem
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        public void ClearAll()
+        {
+            //textBoxWindowDiameter.Clear();
+            //textBoxDoorwayHeight.Clear();
+            //textBoxDoorwayWidth.Clear();
+            labelResult.Content = string.Empty;
+        }
+
+        private void buttonCheck_Click(object sender, RoutedEventArgs e)
+        {
+            // declaring Messages and Titles of the Message Boxes
+            var allParametersMessage = "Please enter all parameters";
+            var zeroMessage = "Size can't be 0!";
+            var errorTitle = "Error!";
+            var resultTitle = "Result";
+            var positiveMessage = "Positive";
+            var negativeMessage = "Negative";
+            // check if all parameters of the fridge were entered
+            if (textBoxFridgeHeight.Text == string.Empty ||
+                textBoxFridgeLength.Text == string.Empty ||
+                textBoxFridgeWidth.Text == string.Empty)
+            {
+                MessageBox.Show(allParametersMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // check if user entered zero as size
+            if (double.Parse(textBoxFridgeHeight.Text) == 0 ||
+                double.Parse(textBoxFridgeLength.Text) == 0 ||
+                double.Parse(textBoxFridgeWidth.Text) == 0)
+            {
+                MessageBox.Show(zeroMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                var checkingFridge = new Fridge(double.Parse(textBoxFridgeHeight.Text),
+                    double.Parse(textBoxFridgeLength.Text),
+                    double.Parse(textBoxFridgeWidth.Text));
+                var check = false;
+                if (comboBox.SelectedItem == comboBox.Items[0])
+                {
+                    // check if all parameters of the doorway were entered
+                    if (textBoxDoorwayHeight.Text == string.Empty ||
+                        textBoxDoorwayWidth.Text == string.Empty)
+                    {
+                        MessageBox.Show(allParametersMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    // check if user entered zero as size
+
+                    if (double.Parse(textBoxDoorwayHeight.Text) == 0 ||
+                        double.Parse(textBoxDoorwayWidth.Text) == 0)
+                    {
+                        MessageBox.Show(zeroMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    check = checkingFridge.GetInCheck(double.Parse(textBoxDoorwayHeight.Text),
+                        double.Parse(textBoxDoorwayWidth.Text));
+                }
+
+                if (comboBox.SelectedItem == comboBox.Items[1])
+                {
+                    // check if all parameters of the window were entered
+                    if (textBoxDoorwayHeight.Text == string.Empty ||
+                        textBoxDoorwayWidth.Text == string.Empty)
+                    {
+                        MessageBox.Show(allParametersMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    // check if user entered zero as size
+
+                    if (double.Parse(textBoxDoorwayHeight.Text) == 0 ||
+                        double.Parse(textBoxDoorwayWidth.Text) == 0)
+                    {
+                        MessageBox.Show(zeroMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    check = checkingFridge.GetInCheck(double.Parse(textBoxDoorwayHeight.Text),
+                        double.Parse(textBoxDoorwayWidth.Text));
+                }
+
+                if (comboBox.SelectedItem == comboBox.Items[2])
+                {
+                    // check if all parameters of the window were entered
+                    if (textBoxWindowDiameter.Text == string.Empty)
+                    {
+                        MessageBox.Show(allParametersMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    // check if user entered zero as size
+
+                    if (double.Parse(textBoxWindowDiameter.Text) == 0)
+                    {
+                        MessageBox.Show(zeroMessage, errorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    check = checkingFridge.GetInCheck(double.Parse(textBoxWindowDiameter.Text));
+                }
+
+                //output results
+                if (check)
+                    //labelResult.Content = "Positive";
+                    MessageBox.Show(positiveMessage, resultTitle, MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+                else
+                    //labelResult.Content = "Negative";
+                    MessageBox.Show(negativeMessage, resultTitle, MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+        }
+
+        private void ComboBoxRectangle_Selected(object sender, RoutedEventArgs e)
+        {
+            //check if components were initialized
+            if (labelWindow == null ||
+                labelDw == null ||
+                textBoxWindowDiameter == null ||
+                textBoxDoorwayHeight == null ||
+                textBoxDoorwayWidth == null ||
+                labelDwHeight == null)
+                return;
+            //make visible components for doorway
+            labelWindow.Visibility = Visibility.Hidden;
+            labelDw.Visibility = Visibility.Visible;
+            labelDwHeight.Visibility = Visibility.Visible;
+            labelWindowDiameter.Visibility = Visibility.Hidden;
+            textBoxWindowDiameter.Visibility = Visibility.Hidden;
+            textBoxDoorwayHeight.Visibility = Visibility.Visible;
+            textBoxDoorwayWidth.Visibility = Visibility.Visible;
+            ClearAll();
+        }
+
+        private void ComboBoxRectangleWindow_Selected(object sender, RoutedEventArgs e)
+        {
+            //check if components were initialized
+            if (labelWindow == null ||
+                labelDw == null ||
+                textBoxWindowDiameter == null ||
+                textBoxDoorwayHeight == null ||
+                textBoxDoorwayWidth == null)
+                return;
+            //make visible components for rectangle window
+            labelDw.Visibility = Visibility.Hidden;
+            labelWindow.Visibility = Visibility.Visible;
+            labelDwHeight.Visibility = Visibility.Visible;
+            labelDwWidth.Visibility = Visibility.Visible;
+            labelWindowDiameter.Visibility = Visibility.Hidden;
+            textBoxWindowDiameter.Visibility = Visibility.Hidden;
+            textBoxDoorwayHeight.Visibility = Visibility.Visible;
+            textBoxDoorwayWidth.Visibility = Visibility.Visible;
+            ClearAll();
+        }
+
+        private void ComboBoxRoundWindow_Selected(object sender, RoutedEventArgs e)
+        {
+            //check if components were initialized
+            if (labelWindow == null ||
+                labelDw == null ||
+                textBoxWindowDiameter == null ||
+                textBoxDoorwayHeight == null ||
+                textBoxDoorwayWidth == null)
+                return;
+            //make visible components for round window
+            labelDw.Visibility = Visibility.Hidden;
+            labelWindow.Visibility = Visibility.Visible;
+            labelDwHeight.Visibility = Visibility.Hidden;
+            labelDwWidth.Visibility = Visibility.Hidden;
+            labelWindowDiameter.Visibility = Visibility.Visible;
+            textBoxWindowDiameter.Visibility = Visibility.Visible;
+            textBoxDoorwayHeight.Visibility = Visibility.Hidden;
+            textBoxDoorwayWidth.Visibility = Visibility.Hidden;
+            ClearAll();
+        }
+
+        protected void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // check if not number => not accept
+            if (e.Text != "," && IsNumber(e.Text) == false)
+            {
+                e.Handled = true;
+            }
+            else if (e.Text == ",")
+            {
+                // check if dot in the beginning of number => not accept
+                if (((TextBox) sender).Text.Length == 0) e.Handled = true;
+                // check if dot occurs ore than once in number => not accept
+                if (((TextBox) sender).Text.IndexOf(e.Text) > -1) e.Handled = true;
+            }
+        }
+
+        private bool IsNumber(string inputText)
+        {
+            return int.TryParse(inputText, out var output);
+        }
+    }
+}
---
+++ FridgeProblemTests/Tests.cs
@@ -0,0 +1,100 @@
+﻿using FridgeProblem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace FridgeProblemTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void FridgeConstructorTest()
+        {
+            const double expectedHeight = 200.0;
+            const double expectedLength = 50.0;
+            const double expectedWidth = 60.0;
+            var actualFridge = new Fridge(200.0, 50.0, 60.0);
+            double[] expected = {expectedHeight, expectedLength, expectedWidth};
+            double[] actual = {actualFridge.Height, actualFridge.Length, actualFridge.Width};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetInRectanglePositiveTest1()
+        {
+            const double dwHeight = 200;
+            const double dwWidth = 60;
+            var testFridge = new Fridge(50, 200, 60);
+            var actualCheck = testFridge.GetInCheck(dwHeight, dwWidth);
+            AreEqual(true, actualCheck);
+        }
+
+        [TestMethod]
+        public void GetInRectanglePositiveTest2()
+        {
+            const double dwHeight = 200;
+            const double dwWidth = 60;
+            var testFridge = new Fridge(60, 50, 200);
+            var actualCheck = testFridge.GetInCheck(dwHeight, dwWidth);
+            AreEqual(true, actualCheck);
+        }
+
+        [TestMethod]
+        public void GetInRectanglePositiveTest3()
+        {
+            const double dwHeight = 200;
+            const double dwWidth = 50;
+            var testFridge = new Fridge(200, 50, 60);
+            bool actualCheck = testFridge.GetInCheck(dwHeight, dwWidth);
+            AreEqual(true, actualCheck);
+        }
+
+        [TestMethod]
+        public void GetInRectangleNegativeTest1()
+        {
+            const double dwHeight = 49;
+            const double dwWidth = 200;
+            var testFridge = new Fridge(200, 50, 60);
+            bool actualCheck = testFridge.GetInCheck(dwHeight, dwWidth);
+            AreEqual(actualCheck, false);
+        }
+
+        [TestMethod]
+        public void GetInRectangleNegativeTest2()
+        {
+            const double dwHeight = 200;
+            const double dwWidth = 49;
+            var testFridge = new Fridge(200, 50, 60);
+            bool actualCheck = testFridge.GetInCheck(dwHeight, dwWidth);
+            AreEqual(actualCheck, false);
+        }
+
+        [TestMethod]
+        public void GetInRectangleNegativeTest3()
+        {
+            const double dwHeight = 49;
+            const double dwWidth = 49;
+            var testFridge = new Fridge(200, 50, 60);
+            bool actualCheck = testFridge.GetInCheck(dwHeight, dwWidth);
+            AreEqual(false, actualCheck);
+        }
+
+        [TestMethod]
+        public void GetInCirclePositiveTest()
+        {
+            const double dwDiameter = 79;
+            var testFridge = new Fridge(50, 200, 60);
+            var actualCheck = testFridge.GetInCheck(dwDiameter);
+            AreEqual(true, actualCheck);
+        }
+
+        [TestMethod]
+        public void GetInCircleNegativeTest()
+        {
+            const double dwDiameter = 78;
+            var testFridge = new Fridge(50, 200, 60);
+            bool actualCheck = testFridge.GetInCheck(dwDiameter);
+            AreEqual(false, actualCheck);
+        }
+    }
+}
